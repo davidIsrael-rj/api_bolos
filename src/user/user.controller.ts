6cd98499587ae93470365864d15f941e357aceb2
DavidIsrael <davidisrael.rj@gmail.com>
2025-10-24 22:36:24 -0300

    Método GET read

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,14 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Get, Post } from "@nestjs/common";
 
 @Controller('users')
 export class UserController{
     @Post()
     async create(@Body() body){
         return{body};
+    }
+
+    @Get()
+    async read(){
+        return {users: []}
     }
 }
