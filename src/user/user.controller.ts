ea1101eda0e40e75e662dd79e9035d5c26777966
DavidIsrael <davidisrael.rj@gmail.com>
2025-11-06 00:33:56 -0300

    Transformação do id string para id numérico

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Put } from "@nestjs/common";
 import { createUserDTO } from "./dto/create-user.dto";
 import { UpdatePutUserDTO } from "./dto/update-put-user.dto";
 import { UpdatePatchUserDTO } from "./dto/update-patch-user.dto";
@@ -16,32 +16,32 @@ export class UserController{
     }
 
     @Get(':id')
-    async readOne(@Param() param){
-        return {user:{}, param}
+    async readOne(@Param('id', ParseIntPipe) id: number){
+        return {user:{}, id}
     }
 
     @Put(':id')
-    async update(@Body() {name,email,password}: UpdatePutUserDTO, @Param() params){
+    async update(@Body() {name,email,password}: UpdatePutUserDTO, @Param('id', ParseIntPipe) id: number){
         return{
             method: 'put',
             name, email, password,
-            params
+            id
         }
     }
 
     @Patch(':id')
-    async updatePartial(@Body() {name,email,password}:UpdatePatchUserDTO, @Param() params){
+    async updatePartial(@Body() {name,email,password}:UpdatePatchUserDTO, @Param('id', ParseIntPipe) id: number){
         return {
             method: 'patch',
             name,email,password,
-            params
+            id
         }
     }
 
     @Delete(':id')
-    async delete(@Param() params){
+    async delete(@Param('id', ParseIntPipe) id: number){
         return{
-            params
+            id
         }
     }
 
