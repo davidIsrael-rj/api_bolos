40b4afde97382727e8f832ed73421ded1995c31a
DavidIsrael <davidisrael.rj@gmail.com>
2025-11-05 23:47:30 -0300

    Estendendo a class UpdatePutUser.dto de CreateUser.dto

--- src/user/dto/update-put-user.dto.ts
+++ src/user/dto/update-put-user.dto.ts
@@ -1,22 +1,3 @@
-import { IsEmail, IsString, IsStrongPassword } from "class-validator";
+import { createUserDTO } from "./create-user.dto";
 
-export class UpdatePutUserDTO{
-    
-    @IsString()
-    name:string;
-
-    @IsEmail()
-    email:string;
-
-    
-    @IsStrongPassword({
-        minLength:6,
-        minLowercase:0,
-        minNumbers:0,
-        minSymbols:0,
-        minUppercase:0,
-    }, {
-        message:"A senha deve conter mais de 6 caracteres"
-    })
-    password:string;
-}
+export class UpdatePutUserDTO extends createUserDTO{}
